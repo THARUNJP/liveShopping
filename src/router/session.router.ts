16b6d577084a23acb1200481f4b6decf9091f7a6
THARUNJP <tharunroshini71@gmail.com>
2025-12-13 16:15:42 +0530

    c

--- src/router/session.router.ts
+++ src/router/session.router.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
+import { createSession } from "../controller/session.controller";
 const router = Router();
 
-router.post("/create",()=>{})
+router.post("/create",createSession)
 
 export default router;
