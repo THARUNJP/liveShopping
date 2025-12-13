19791392a0b93ce1bb1b51aec6949fe059638bdc
THARUNJP <tharunroshini71@gmail.com>
2025-12-13 19:01:09 +0530

    socket intial file completed

--- src/socket/init.socket.ts
+++ src/socket/init.socket.ts
@@ -1,14 +1,31 @@
 import { Server } from "socket.io";
-import { Server as HttpServer } from "node:http";
+import { Server as HttpServer } from "http";
 
-let io;
-function initSocket(server: HttpServer) {
+let io: Server;
+
+export function initSocket(server: HttpServer): Server {
   io = new Server(server, {
     cors: {
       origin: "http://localhost:3000",
       methods: ["GET", "POST"],
     },
   });
+
+  io.on("connection", (socket) => {
+    console.log("Socket connected:", socket.id);
+
+    socket.on("disconnect", () => {
+      console.log("Socket disconnected:", socket.id);
+    });
+  });
+
+  return io;
 }
 
-export default initSocket;
+// Optional getter (useful later)
+export function getIO(): Server {
+  if (!io) {
+    throw new Error("Socket.io not initialized");
+  }
+  return io;
+}
